256cd862dd110ec4fb411e8e069992daee2deca4
fcc-code-7 <faraziqbalmuhammad@gmail.com>
2024-05-05 15:27:03 +0500

    Updated Views

--- Menu/Menu/obj/Debug/net8.0/Menu.AssemblyInfo.cs
+++ Menu/Menu/obj/Debug/net8.0/Menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928dd5463734bafc3bcf6151e8472443c34964f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284de51b6c56ac9ba3affef8af8447f29465b954")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
